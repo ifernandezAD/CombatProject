a69384c04087c7b2e484fb755cd5e578fc9d1031
IFernandezAD <ifernandezad@gmail.com>
2023-11-06 21:42:26 +0100

    Input Controller
    Started setting up the input system

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -1,18 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    //[Header("Movement")]
+    //public enum MovementMode 
+    //{
+    //    PlayerDirection,
+    //    CameraDirection,
+    //}
+    //[SerializeField] float speed = 4f;
+    //
+   // [Header("Input"]
+    [SerializeField] InputActionReference jump;
+    [SerializeField] InputActionReference move;
+
+    CharacterController characterController;
+    
+
+    private void Awake()
+    {
+        characterController =GetComponentInChildren<CharacterController>();
+    }
+
+    private void OnEnable()
+    {
+        move.action.Enable();
+        jump.action.Enable();
+    }
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 rawMoveValue = move.action.ReadValue<Vector2>();
+        Vector3 xzPlanetMovement = (Vector3.right * rawMoveValue.x) + (Vector3.forward * rawMoveValue.y);
+
+        //characterController.Move(rawMoveValue * speed * Time.deltaTime);
     }
+
+    private void OnDisable()
+    {
+        move.action.Disable();
+        jump.action.Disable();
+    }
+
 }
