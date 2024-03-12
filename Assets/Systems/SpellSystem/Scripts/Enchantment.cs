8b19f4c2662c46cff3a9ba4563e2789b006c2dab
IFernandezAD <ifernandezad@gmail.com>
2024-03-12 16:21:12 +0100

    Enchantment Spell Basic Behaviour Added

--- Assets/Systems/SpellSystem/Scripts/Enchantment.cs
+++ Assets/Systems/SpellSystem/Scripts/Enchantment.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Enchantment : Spell
 {
+    [Header("Animations")]
+    private readonly int enchantmentHash = Animator.StringToHash("EnchantmentSpell");
 
-    protected override void SetSpellAnimation()
-    {
-        animator.SetTrigger("HypnosisSpell");
-    }
-
+    [Header("Enchantment Spell")]
+    [SerializeField] private SimpleAudioEvent enchantmentAudioEvent;
+    [SerializeField] private float animationDuration = 4f;
+      
     protected override void BeginSpell()
     {
-        //Nothing Yet
+        PlayAbjurationSound();
+        DOVirtual.DelayedCall(animationDuration, entityWeapons.RecoverWeapon);
+        DOVirtual.DelayedCall(animationDuration, EnablePlayerController);
     }
+
+    protected override void SetSpellAnimation() {animator.SetTrigger(enchantmentHash);}
+
+    public void PlayAbjurationSound() { enchantmentAudioEvent.Play(voiceAudioSource); }
 
     protected override void EndSpell()
     {
