930dde607ad0241202ac3fe99eea030bd0978b04
IFernandezAD <ifernandezad@gmail.com>
2024-03-15 11:12:11 +0100

    Enchantment bug solved
    Confused players can now dance

--- Assets/Systems/AI/States/Scripts/State_Enchanted.cs
+++ Assets/Systems/AI/States/Scripts/State_Enchanted.cs
@@ -28,17 +28,17 @@ public class State_Enchanted : StateBase
     }
 
     void PlayEnchantedAnimation()
-    {
-        if (ai.senseable.allegiance == "Gangster")
+    {      
+        if ((ai.senseable.allegiance == "Gangster")||(ai.senseable.allegiance == "Player")) //Player means a skin changed enemy
         {
-            int randomAnim = UnityEngine.Random.Range(1, 6);
+            int randomAnim = UnityEngine.Random.Range(1, 5);
             ai.animator.SetInteger(gangsterEnchantmentHash, randomAnim);
         }
     }
 
     void StopEnchantedAnimation()
     {
-        if (ai.senseable.allegiance == "Gangster")
+        if ((ai.senseable.allegiance == "Gangster") || (ai.senseable.allegiance == "Player"))
         {
             ai.animator.SetInteger(gangsterEnchantmentHash, 0);
         }
