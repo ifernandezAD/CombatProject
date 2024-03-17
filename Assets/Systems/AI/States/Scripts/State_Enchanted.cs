af00140142663eadf6150b56de68fbc58e2aa702
IFernandezAD <ifernandezad@gmail.com>
2024-03-17 17:43:36 +0100

    Summon can dance

--- Assets/Systems/AI/States/Scripts/State_Enchanted.cs
+++ Assets/Systems/AI/States/Scripts/State_Enchanted.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class State_Enchanted : StateBase
 {
     [Header("EnchantmentAnimations")]
-    private readonly int gangsterEnchantmentHash = Animator.StringToHash("EnchantmentType");
+    private readonly int enchantmentHash = Animator.StringToHash("EnchantmentType");
 
     [SerializeField] float enchantedDuration = 10f;
     float currentTime;
@@ -28,19 +28,22 @@ public class State_Enchanted : StateBase
     }
 
     void PlayEnchantedAnimation()
-    {      
-        if ((ai.senseable.allegiance == "Gangster")||(ai.senseable.allegiance == "Player")) //Player means a skin changed enemy
+    {
+        if ((ai.senseable.allegiance == "Gangster") || (ai.senseable.allegiance == "Player")) //Player means a skin changed enemy
         {
             int randomAnim = UnityEngine.Random.Range(1, 5);
-            ai.animator.SetInteger(gangsterEnchantmentHash, randomAnim);
+            ai.animator.SetInteger(enchantmentHash, randomAnim);
+        }
+        else
+        {
+            ai.animator.SetInteger(enchantmentHash, 1);
         }
     }
 
     void StopEnchantedAnimation()
     {
-        if ((ai.senseable.allegiance == "Gangster") || (ai.senseable.allegiance == "Player"))
-        {
-            ai.animator.SetInteger(gangsterEnchantmentHash, 0);
-        }
+
+        ai.animator.SetInteger(enchantmentHash, 0);
+
     }
 }
