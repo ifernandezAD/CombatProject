00b56e5362542085dddf81ed8e5c50e48c8e07a2
IFernandezAD <ifernandezad@gmail.com>
2024-03-18 16:21:43 +0100

    Start working on camera alignment

--- Assets/!Player/Scripts/PlayerCameraAligner.cs
+++ Assets/!Player/Scripts/PlayerCameraAligner.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class PlayerCameraAligner : MonoBehaviour
 {
-   //[SerializeField] CinemachineFreeLook[] cameras;
-   //CinemachineBrain brain;
-   //
-   //private void Awake()
-   //{
-   //    brain = Camera.main.GetComponent<CinemachineBrain>();
-   //}
-   //
-   //private void Update()
-   //{
-   //    if (cameras.Contains(brain.ActiveVirtualCamera))
-   //    {
-   //        foreach (ICinemachineCamera camera in cameras)
-   //        {
-   //            if (camera != brain.ActiveVirtualCamera)
-   //            {
-   //                if (camera is CinemachineFreeLook)
-   //                {
-   //                    CinemachineFreeLook freeLook = camera as CinemachineFreeLook;
-   //                    freeLook.m_YAxis.Value = 
-   //                }
-   //            }
-   //        }
-   //    }
-   //}
+   [SerializeField] CinemachineVirtualCameraBase[] cameras;
+   CinemachineBrain brain;
+   
+   private void Awake()
+   {
+       brain = Camera.main.GetComponent<CinemachineBrain>();
+   }
+   
+   private void Update()
+   {
+       if (cameras.Contains(brain.ActiveVirtualCamera))
+       {
+           foreach (ICinemachineCamera camera in cameras)
+           {
+               if (camera != brain.ActiveVirtualCamera)
+               {
+                   if (camera is CinemachineFreeLook)
+                   {
+                       CinemachineFreeLook freeLook = camera as CinemachineFreeLook;
+                       //freeLook.m_YAxis.Value = 
+                   }
+               }
+           }
+       }
+   }
 }
