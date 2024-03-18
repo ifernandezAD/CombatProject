c3514661302cecee46f95eaa39846340e891eca8
IFernandezAD <ifernandezad@gmail.com>
2024-03-18 17:53:50 +0100

    Summon Spell Logic Added

--- Assets/Systems/SpellSystem/Scripts/EvocationSpell.cs
+++ Assets/Systems/SpellSystem/Scripts/EvocationSpell.cs
@@ -1,21 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class EvocationSpell : Spell
 {
-    protected override void SetSpellAnimation()
-    {
-        //Nothing Yet
-    }
+
+
+    [Header("Animations")]
+    private readonly int evocationHash = Animator.StringToHash("EvocationSpell");
+    
+
+    [Header("Evocation Spell")]
+    [SerializeField] float animationDuration = 3.5f;
+    [SerializeField] float summonDelay = 2.5f;
+    [SerializeField] GameObject summonPrefab;
+    private GameObject summonClone;
+    [SerializeField] Transform summonInstantationPosition;
 
     protected override void BeginSpell()
     {
-        //Nothing Yet
+        DOVirtual.DelayedCall(summonDelay, InstantiateDemon);
+        DOVirtual.DelayedCall(animationDuration, entityWeapons.RecoverWeapon);
+        DOVirtual.DelayedCall(animationDuration, EnablePlayerController);
+    }
+
+    void InstantiateDemon()
+    {
+       summonClone=Instantiate(summonPrefab, summonInstantationPosition.position, summonInstantationPosition.rotation);
+    }
+
+    protected override void SetSpellAnimation()
+    {
+        animator.SetTrigger(evocationHash);
     }
 
     protected override void EndSpell()
     {
-        //Nothing yet
+        Destroy(summonClone);
     }
 }
