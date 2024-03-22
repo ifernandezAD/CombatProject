1b11cf1f215ff87aca1e0ec866a08f04e96d52d8
IFernandezAD <ifernandezad@gmail.com>
2024-03-22 15:46:17 +0100

    New debug navmesh and player pos

--- Assets/Gameplay/GameLogic/Scripts/GameLogic.cs
+++ Assets/Gameplay/GameLogic/Scripts/GameLogic.cs
@@ -1,28 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [DefaultExecutionOrder(-100)]
 public class GameLogic : MonoBehaviour
 {
+    public static GameLogic instance { get; private set; }
 
-   public static GameLogic instance { get; private set; }
+    public Transform playerTransfrom { get; private set; }
 
 
     private void Awake()
     {
-        instance = this;   
+        instance = this;
+
+        playerTransfrom = GameObject.FindWithTag("Player").transform;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
