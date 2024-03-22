49e528fb0d5cf785ecf4ede8fd8c5eaad4108926
IFernandezAD <ifernandezad@gmail.com>
2024-03-22 16:58:30 +0100

    Adversary found you by audible

--- Assets/Gameplay/GameLogic/Scripts/GameLogic.cs
+++ Assets/Gameplay/GameLogic/Scripts/GameLogic.cs
@@ -5,8 +5,21 @@ public class GameLogic : MonoBehaviour
 {
     public static GameLogic instance { get; private set; }
 
+    [SerializeField] bool isRagnarokMode;
+
     public Transform playerTransfrom { get; private set; }
 
+    [Header("Debug")]
+    [SerializeField] bool debugEnterRagnarokMode;
+
+    private void OnValidate()
+    {
+        if (debugEnterRagnarokMode)
+        {
+            EnterRagnarokMode();
+            debugEnterRagnarokMode = false;
+        }
+    }
 
     private void Awake()
     {
@@ -15,5 +28,9 @@ public class GameLogic : MonoBehaviour
         playerTransfrom = GameObject.FindWithTag("Player").transform;
     }
 
-
+    void EnterRagnarokMode()
+    {
+        AudibleByConstantEmission audibleByAdversary = playerTransfrom.GetComponent<AudibleByConstantEmission>();
+        audibleByAdversary.enabled = true;
+    }
 }
