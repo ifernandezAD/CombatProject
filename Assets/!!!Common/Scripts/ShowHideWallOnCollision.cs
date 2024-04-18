bc9ac60dc08408ba9bdbc5527cc5570d0b17325c
IFernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2024-04-18 18:34:00 +0200

    Magic wall Working

--- Assets/!!!Common/Scripts/ShowHideWallOnCollision.cs
+++ Assets/!!!Common/Scripts/ShowHideWallOnCollision.cs
@@ -1,30 +1,22 @@
-using DG.Tweening;
 using UnityEngine;
 
 public class ShowHideWallOnCollision : MonoBehaviour
 {
-    public Material material;
-    public string floatPropertyName = "Transparency"; 
-    public float targetValue = 1f;
-    public float transitionDuration = 1f;
-    private bool isPlayerInside = false;
+    [SerializeField] private GameObject visuals;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            isPlayerInside = true;
-            // Start transition from current value to target value
-            DOTween.To(() => material.GetFloat(floatPropertyName), x => material.SetFloat(floatPropertyName, x), targetValue, transitionDuration);
+            visuals.SetActive(true);
         }
     }
-    
 
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-
+            visuals.SetActive(false);
         }
     }
 }
