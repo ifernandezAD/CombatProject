afb55b214e9d55a39fa0f6a0f771ca466103e284
IFernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2024-04-19 13:03:15 +0200

    Progress on Vfx particles spawning as feedback

--- Assets/!Entities/Scripts/EntityParticles.cs
+++ Assets/!Entities/Scripts/EntityParticles.cs
@@ -1,12 +1,12 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EntityParticles : MonoBehaviour
 {
-    [SerializeField] GameObject bloodParticle;
     private HurtCollider hurtCollider;
+
+    [Header("Particles")]
+    [SerializeField] GameObject bloodParticle;
+    [SerializeField] GameObject meleeParticle;
 
     private void Awake()
     {
@@ -20,10 +20,18 @@ public class EntityParticles : MonoBehaviour
 
     private void InstantiateParticle(IOffender offender)
     {
-       if (offender.GetWeaponType() == IOffender.WeaponType.fireWeapon)
+        Vector3 spawnPosition = transform.position + transform.up;
+
+        if (offender.GetWeaponType() == IOffender.WeaponType.fireWeapon)
         {
-          GameObject bloodParticleClone = Instantiate(bloodParticle, transform.position, Quaternion.identity);
-          Destroy(bloodParticleClone, 1);
+            GameObject bloodParticleClone = Instantiate(bloodParticle, spawnPosition, Quaternion.identity);
+            Destroy(bloodParticleClone, 1);
+        }
+
+        if (offender.GetWeaponType() == IOffender.WeaponType.meleeWeapon)
+        {
+            GameObject meleeParticleClone = Instantiate(meleeParticle, spawnPosition, Quaternion.identity);
+            Destroy(meleeParticleClone, 1);
         }
     }
 
