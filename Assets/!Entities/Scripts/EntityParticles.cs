30475c7044ca18c3993bac1e83a38c123ff6ff57
IFernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2024-04-22 18:32:58 +0200

    Feedback particles working

--- Assets/!Entities/Scripts/EntityParticles.cs
+++ Assets/!Entities/Scripts/EntityParticles.cs
@@ -7,6 +7,7 @@ public class EntityParticles : MonoBehaviour
     [Header("Particles")]
     [SerializeField] GameObject bloodParticle;
     [SerializeField] GameObject meleeParticle;
+    [SerializeField] GameObject fireParticle;
 
     private void Awake()
     {
@@ -22,7 +23,7 @@ public class EntityParticles : MonoBehaviour
     {
         Vector3 spawnPosition = transform.position + transform.up;
 
-        if (offender.GetWeaponType() == IOffender.WeaponType.fireWeapon)
+        if ((offender.GetWeaponType() == IOffender.WeaponType.fireWeapon) || (offender.GetWeaponType() == IOffender.WeaponType.explosionWeapon))
         {
             GameObject bloodParticleClone = Instantiate(bloodParticle, spawnPosition, Quaternion.identity);
             Destroy(bloodParticleClone, 1);
@@ -33,6 +34,13 @@ public class EntityParticles : MonoBehaviour
             GameObject meleeParticleClone = Instantiate(meleeParticle, spawnPosition, Quaternion.identity);
             Destroy(meleeParticleClone, 1);
         }
+
+        if (offender.GetWeaponType() == IOffender.WeaponType.flamesWeapon)
+        {
+            GameObject flamesParticleClone = Instantiate(fireParticle, spawnPosition, Quaternion.identity);
+            Destroy(flamesParticleClone, 1);
+        }
+
     }
 
     private void OnDisable()
