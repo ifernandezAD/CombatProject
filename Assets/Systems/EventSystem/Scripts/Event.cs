ad33782ea0522a80f5dac627de2865fc32776ea8
IFernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2024-04-25 10:01:10 +0200

    TutorialEvents display texts array

--- Assets/Systems/EventSystem/Scripts/Event.cs
+++ Assets/Systems/EventSystem/Scripts/Event.cs
@@ -1,20 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using DG.Tweening;
 
 public class Event : MonoBehaviour
 {
+    [Header("Show Text Variables")]
     [SerializeField] string[] eventTextArray;
-    [SerializeField] TextMeshProUGUI eventText;
+    [SerializeField] float textDisplayTime = 1f;
 
-    private void Awake()
-    {
-        //Tiraremos por el GetSiblingIndex
-    }
+    private EventManager eventManager;
 
     private void OnEnable()
     {
-        
+        ShowTextsByTime();
+    }
+
+    private int currentTextIndex = 0;
+    private void ShowTextsByTime()
+    {
+        if (eventTextArray.Length == 0)
+            return;
+
+        ShowTextDelayed(currentTextIndex);
+    }
+
+    private void ShowTextDelayed(int index)
+    {
+        if (index < 0 || index >= eventTextArray.Length)
+            return;
+
+        eventManager.eventTMPro.text = eventTextArray[index];
+
+        currentTextIndex++;
+        if (currentTextIndex < eventTextArray.Length)
+        {
+            DOTween.Sequence()
+                .AppendInterval(textDisplayTime)
+                .OnComplete(() => ShowTextDelayed(currentTextIndex));
+        }
+    }
+
+    public void Init(EventManager eventManager)
+    {
+        this.eventManager = eventManager;
     }
 }
--- Assets/Systems/EventSystem/Scripts/EventManager.cs
+++ Assets/Systems/EventSystem/Scripts/EventManager.cs
@@ -1,13 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EventManager : MonoBehaviour
 {
-    [SerializeField] private Transform eventsParent;
+    [SerializeField] Transform eventsParent;
+    public TextMeshProUGUI eventTMPro;
 
-    private void Start()
+    private Event[] eventArray;
+
+    private void Awake()
     {
-        eventsParent.GetChild(0).gameObject.SetActive(true);
+        InitializeEvents();
+    }
+
+    private void InitializeEvents()
+    {
+        Event[] eventsInChildren = eventsParent.GetComponentsInChildren<Event>();
+
+        eventArray = new Event[eventsInChildren.Length];
+
+        for (int i = 0; i < eventsInChildren.Length; i++)
+        {
+            Event currentEvent = eventsInChildren[i];
+            currentEvent.Init(this);
+            eventArray[i] = currentEvent;
+        }
     }
 }
